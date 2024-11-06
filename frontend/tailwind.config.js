49d64902ea22eba6348848316b63f89e56f548a7
neco8 <shogibeshow@gmail.com>
2024-11-06 18:39:57 +0900

    init: プロジェクトを初期化
    README / configファイル

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{elm,ts}", "./styles/**/*.{scss,css}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
