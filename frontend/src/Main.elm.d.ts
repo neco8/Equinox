d72ffb3b921f1bc7febbd263908cd92704ed1f12
neco8 <shogibeshow@gmail.com>
2024-11-07 08:56:09 +0900

    add: Main.elmの型定義追加
    Elmのポート通信のためのPortsインターフェースを含む実装を提供。Elmアプリケーションの初期化に必要なインターフェースを記述した。

---
+++ frontend/src/Main.elm.d.ts
@@ -0,0 +1,112 @@
+import { StorageQuerySDL } from "./typescript/storage";
+
+export type Category = {
+  id: string;
+  title: string;
+};
+
+export type BreathingMethod = {
+  id: string;
+  name: string;
+  "category-id": string;
+  "created-at": number;
+  inhale: number;
+  "inhale-hold": number;
+  exhale: number;
+  "exhale-hold": number;
+};
+
+export type Session = {
+  id: string;
+  "breathing-method-id": string;
+  "breathing-method-name": string;
+  inhale: number;
+  "inhale-hold": number;
+  exhale: number;
+  "exhale-hold": number;
+  duration: number;
+  "created-at": number;
+};
+
+// Query Result Types
+
+// [ ] TODO: このような型が抽象的なクエリリザルトはいらない。CategoryやBreathingMethod等に合わせて、それぞれ愚直にリザルトを作成する
+// [ ] TODO: ポートの命名規則がほしい
+// [ ] TODO: ポートで、elmで設定したポートがすべてd.tsに存在するか確認する機構がほしい
+
+export type Entity = "category" | "breathing-method" | "session";
+
+// - [ ] TODO: これ多分いらない
+export type QueryListResult =
+  | {
+      type: "category-list";
+      data: unknown[];
+    }
+  | {
+      type: "breathing-method-list";
+      data: unknown[];
+    }
+  | {
+      type: "session-list";
+      data: unknown[];
+    };
+
+export type QuerySingleResult = {
+  type: "entity-single";
+  entity: Entity;
+  data: Category | BreathingMethod | Session;
+};
+
+export type QueryResult = QueryListResult | QuerySingleResult;
+
+// Errorタイプとかも、定義自身に確固たる理由がなく、ただ起こりそうだからぐらい。デコードはelmしか知ってはいけない知識だからここにあるのはおかしいよね。
+// Error Types
+export type QueryError =
+  | { type: "NotFound" }
+  | { type: "DecodingError"; message: string }
+  | { type: "QueryError"; message: string }
+  | { type: "UnknownError"; message: string };
+
+// これはまああり。
+// Save Result Type
+export type SaveResult =
+  | { type: "success" }
+  | { type: "error"; message: string };
+
+// Command and Subscription Types
+export type Cmd<T> = {
+  subscribe: (fn: (value: T) => void) => void;
+};
+
+export type Sub<T> = {
+  send: (value: T) => void;
+};
+
+// ポート系怪しい。
+// Port Interfaces
+export interface Ports {
+  queryStorage: Cmd<StorageQuerySDL>;
+  receiveQueryResult: Sub<QueryResult>;
+  receiveQueryError: Sub<QueryError>;
+  // save系は、結構ありだと思う。でも、複数保存のユースケースは要らない？今のところは要らないからきっとYAGNIなんだろうが、不安だ。
+  saveBreathingMethod: Cmd<BreathingMethod>;
+  saveCategory: Cmd<Category>;
+  saveSession: Cmd<Session>;
+  // [ ] TODO: saveResultについても実装する
+  // UUID
+  generateUuid: Cmd<void>;
+  receiveUuid: Sub<string>;
+}
+
+// Elm App Interface
+export namespace Elm {
+  export namespace Main {
+    export interface App {
+      ports: Ports;
+    }
+    export function init(param: {
+      node: HTMLElement;
+      flags: number /** 現在時刻 */;
+    }): Elm.Main.App;
+  }
+}
