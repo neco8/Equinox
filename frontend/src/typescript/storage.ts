75aa8567d7018ad9d6c47a01de2d8b79db11465a
neco8 <shogibeshow@gmail.com>
2024-11-07 08:52:12 +0900

    add: ストレージのインターフェース
    ストレージインターフェース（Storage）として保存とクエリ用のメソッドのプロミスを定義。

---
+++ frontend/src/typescript/storage.ts
@@ -0,0 +1,40 @@
+import { QueryListResult, QuerySingleResult, SaveResult } from "../Main.elm";
+
+// 以下のエラークラスを送出すること。
+
+export class NotFoundError extends Error {
+  constructor() {
+    super("Not Found");
+    this.name = "NotFoundError";
+  }
+}
+
+export class QueryError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "QueryError";
+  }
+}
+
+export type StorageKey = "EQUINOX_breathing-methods" | "EQUINOX_categories" | "EQUINOX_sessions";
+
+// SDL Query Types
+export type Operator = "eq" | "gt" | "lt" | "gte" | "lte";
+
+export interface Condition {
+  field: string;
+  operator: Operator;
+  value: string | number | boolean;
+}
+
+export interface StorageQuerySDL {
+  storageKey: StorageKey
+  conditions: Condition[] | undefined;
+}
+
+// Storage Interface
+export interface Storage {
+  save(key: StorageKey, value: unknown): Promise<SaveResult>;
+  queryAll(key: StorageKey, query: StorageQuerySDL): Promise<QueryListResult>;
+  queryOne(key: StorageKey, query: StorageQuerySDL): Promise<QuerySingleResult>;
+}
