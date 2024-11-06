dd1e7f59dd59e73d018595187e8a289c0ba5b707
neco8 <shogibeshow@gmail.com>
2024-11-07 08:30:29 +0900

    add: QueryErrorモジュールの追加
    ストレージ操作中のエラーを表現する型とデコーダーを提供するQueryErrorモジュールを新規作成。主なエラー種別（NotFound、DecodingError、QueryError、UnknownError）を定義し、各種ストレージを操作する際に起こり得るエラーを適切にデコードする。

---
+++ frontend/src/JS/Storage/QueryError.elm
@@ -0,0 +1,70 @@
+module JS.Storage.QueryError exposing (..)
+
+{-| `JS.Storage.QueryError` モジュールは、ストレージ操作中に発生しうるエラーを表現するための型とデコードするための関数を定義します。
+
+このモジュールは、特にJSONを介したストレージアクセスで、エラーを適切にハンドリングしやすくすることを目的としています。
+
+
+### 型
+
+@docs QueryError
+
+
+### デコーダー
+
+@docs queryErrorDecoder
+
+-}
+
+import Json.Decode as D
+
+
+{-| `QueryError`は、ストレージ操作中に発生する可能性があるエラーを表す型です。
+
+  - `NotFound`: データが見つからないエラーを表します。
+  - `DecodingError`: JSONデコード中に発生したエラーを表し、詳細なエラーメッセージが含まれます。
+  - `QueryError`: クエリに関連するエラーで、原因を示すメッセージが含まれます。
+  - `UnknownError`: 未知のエラーで、追加の情報がメッセージとして提供されます。
+
+-}
+type QueryError
+    = NotFound
+    | DecodingError String
+    | QueryError String
+    | UnknownError String
+
+
+{-| `queryErrorDecoder` は、JavaScriptからのJSONエラー情報を `QueryError` 型にデコードするためのデコーダーです。
+JSONオブジェクトの `"type"` の値に基づいて対応する `QueryError` を生成します。
+
+    - "NotFound": { "type": "NotFound" } というオブジェクトを `NotFound` エラーとしてデコードします。
+    - "DecodingError": { "type": "DecodingError", "message": "エラーメッセージ" } というオブジェクトを `DecodingError` エラーとしてデコードします。
+    - "QueryError": { "type": "QueryError", "message": "エラーメッセージ" } というオブジェクトを `QueryError` エラーとしてデコードします。
+    - "UnknownError": { "type": "UnknownError", "message": "エラーメッセージ" } というオブジェクトを `UnknownError` エラーとしてデコードします。
+    - 不明な値が `"type"` フィールドに含まれている場合、`UnknownError`として `"Unknown error"` のメッセージを付加してデコードされます。
+
+-}
+queryErrorDecoder : D.Decoder QueryError
+queryErrorDecoder =
+    D.field "type" D.string
+        |> D.andThen
+            (\type_ ->
+                case type_ of
+                    "NotFound" ->
+                        D.succeed NotFound
+
+                    "DecodingError" ->
+                        D.field "message" D.string
+                            |> D.map DecodingError
+
+                    "QueryError" ->
+                        D.field "message" D.string
+                            |> D.map QueryError
+
+                    "UnknownError" ->
+                        D.field "message" D.string
+                            |> D.map UnknownError
+
+                    _ ->
+                        D.succeed (UnknownError "Unknown error")
+            )
