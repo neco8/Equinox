ceaeab6fd1d0b1e78ffd53b1a7fdc8eda20b0b69
neco8 <shogibeshow@gmail.com>
2024-11-13 11:42:57 +0900

    refactor: 呼吸法の秒数を入力するための入力コンポーネントを共通化
    呼吸法の入力のためのコンポーネントを共通化しました。ManualSessionPreparationと、BreathingMethodAdd / Editにて利用されます。

---
+++ frontend/src/BreathingMethodDurationInput.elm
@@ -0,0 +1,52 @@
+module BreathingMethodDurationInput exposing (Config, view)
+
+{-| このモジュールは、呼吸法の秒数をセットするためのコンポーネントです。
+
+ManualSessionPreparationPageとBreathingMethodAddPageの間で共通化を図ります。
+-}
+
+import Html exposing (Html, div, input)
+import Html.Attributes exposing (attribute, value)
+import Html.Events exposing (onInput)
+
+
+{-| BreathingMethodDurationInputを使用する際に必要な設定を定義します。
+-}
+type alias Config msg =
+    { onInputInhaleDuration : String -> msg
+    , onInputInhaleHoldDuration : String -> msg
+    , onInputExhaleDuration : String -> msg
+    , onInputExhaleHoldDuration : String -> msg
+    }
+
+
+{-| BreathingMethodDurationInputのビューを生成します。
+-}
+view : Config msg -> { model | inhaleDurationInput : String, inhaleHoldDurationInput : String, exhaleDurationInput : String, exhaleHoldDurationInput : String } -> Html msg
+view config model =
+    div []
+        [ input
+            [ attribute "aria-label" "inhale-duration-input"
+            , onInput config.onInputInhaleDuration
+            , value model.inhaleDurationInput
+            ]
+            []
+        , input
+            [ attribute "aria-label" "inhale-hold-duration-input"
+            , onInput config.onInputInhaleHoldDuration
+            , value model.inhaleHoldDurationInput
+            ]
+            []
+        , input
+            [ attribute "aria-label" "exhale-duration-input"
+            , onInput config.onInputExhaleDuration
+            , value model.exhaleDurationInput
+            ]
+            []
+        , input
+            [ attribute "aria-label" "exhale-hold-duration-input"
+            , onInput config.onInputExhaleHoldDuration
+            , value model.exhaleHoldDurationInput
+            ]
+            []
+        ]
