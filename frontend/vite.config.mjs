f7e16f236618ef64a2af64f7d76001cfee83c0e9
neco8 <shogibeshow@gmail.com>
2024-11-13 17:44:39 +0900

    add: GitHub Pages デプロイ対応
    GitHub Pagesにてデプロイするためのコードを追加

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -3,8 +3,13 @@ import elmPlugin from 'vite-plugin-elm';
 import sassPlugin from 'vite-plugin-sass';
 
 export default defineConfig({
+  base: './', // GitHubページでの相対パスを正しく解決するため
   plugins: [
     elmPlugin(),
     sassPlugin()
-  ]
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 });
