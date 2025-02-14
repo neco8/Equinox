803116a97eb88940e81b0efd2a4db8a4460656d6
neco8 <shogibeshow@gmail.com>
2025-02-15 03:48:46 +0900

    fix: tailwind srcを修正

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{elm,ts}", "./styles/**/*.{scss,css}", "./index.html"],
+  content: ["./**/*.{elm,ts}", "./styles/**/*.{scss,css}", "./index.html"],
   theme: {
     extend: {},
   },
